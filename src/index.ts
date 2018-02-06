366d8ea75e3be433fdcc084a01cc8486868e25c9
blackcater <blackcater2015@gmail.com>
2018-02-06 19:47:41 +0800

    Storage temporarily

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,76 @@
  * @author: blackcater<blackcater2015@gmail.com>
  */
 
-export function add(a: number = 0, b: number = 0) {
-  return a + b
+// thenable
+interface IPromiseThenable {
+  then: () => any
+}
+
+type PromiseStateType = 'pending' | 'fulfilled' | 'rejected'
+type PromiseCallbackType = () => any
+type PromiseCallbackListType = PromiseCallbackType[]
+
+function nextTick(cb, args, context) {
+  if (process) {
+    process.nextTick(() => cb.apply(context, args))
+  } else if (setImmediate) {
+    setImmediate(() => cb.apply(context, args))
+  } else {
+    setTimeout(() => cb.apply(context, args), 0)
+  }
+}
+
+export default class Promise {
+  // 存储结果值
+  private value: any
+  // 成功回调事件
+  private fulfilledCbs: PromiseCallbackListType = []
+  // 失败回调事件
+  private rejectedCbs: PromiseCallbackListType = []
+  // 当前 Promise 状态
+  private state: PromiseStateType = 'pending'
+
+  /**
+   * Promise then 方法
+   *
+   * @param resolve
+   * @param reject
+   */
+  public then(resolve, reject) {
+    if (typeof resolve === 'function') {
+      this.fulfilledCbs.push(resolve)
+    }
+
+    if (typeof reject === 'function') {
+      this.rejectedCbs.push(reject)
+    }
+
+    if (this.state === 'fulfilled') {
+      // fulfilled
+      this._resolve()
+    }
+
+    if (this.state === 'rejected') {
+      // rejected
+      this._reject()
+    }
+  }
+
+  private _resolve() {
+    for (let i = 0, len = this.fulfilledCbs.length; i < len; i++) {
+      nextTick(this.fulfilledCbs[i], [this.value], {})
+    }
+
+    this.fulfilledCbs = []
+    this.rejectedCbs = []
+  }
+
+  private _reject() {
+    for (let i = 0, len = this.rejectedCbs.length; i < len; i++) {
+      nextTick(this.rejectedCbs[i], [this.value], {})
+    }
+
+    this.fulfilledCbs = []
+    this.rejectedCbs = []
+  }
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,9 @@
-import { add } from '../src/'
+import Promise from '../src/'
 
-describe('@index', () => {
-  describe('#add', () => {
-    it('should return 0', () => {
-      expect(add()).toBe(0)
-    })
-    it('should return right result', () => {
-      expect(add(1, 1)).toBe(2)
-      expect(add(1, 2)).toBe(3)
+describe('Promise', () => {
+  describe('@constructor', () => {
+    test('init with value', () => {
+      expect(1).toBe(1)
     })
   })
 })
