b90f1b7d606e94d13d1baeb281ba150c354ba630
blackcater <blackcater2015@gmail.com>
2018-02-07 18:29:21 +0800

    Pick `procedure` outside Class Promise

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,15 @@ type PromiseStateType = 'pending' | 'fulfilled' | 'rejected'
 type PromiseCallbackType = (value: any) => any
 type PromiseCallbackListType = PromiseCallbackType[]
 
+function isPromise(x) {
+  return x instanceof Promise
+}
+
+function isThenable(x) {
+  return x && x.then && typeof x.then === 'function'
+}
+
+// 异步调用方法 cb
 function nextTick(cb) {
   if (!cb) return
 
@@ -23,6 +32,60 @@ function nextTick(cb) {
     setImmediate(cb)
   } else {
     setTimeout(cb, 0)
+  }
+}
+
+// 处理 下一个 promise
+function procedure(promise: Promise, x: any) {
+  if (promise === x) {
+    return promise._reject(new TypeError('promise and value cannot be same'))
+  }
+
+  if (isPromise(x)) {
+    return procedurePromise(promise, x)
+  }
+
+  if (isThenable(x)) {
+    return procedureThenable(promise, x)
+  }
+
+  return promise._resolve(x)
+}
+
+// 处理 promise 类型值
+function procedurePromise(promise: Promise, x: Promise) {
+  if (x.state === 'fulfilled') {
+    promise._resolve(x.value)
+  }
+
+  if (x.state === 'rejected') {
+    promise._reject(x.value)
+  }
+
+  x.then(promise._resolve.bind(promise), promise._reject.bind(promise))
+}
+
+// 处理 thenable 类型值
+function procedureThenable(promise: Promise, x: IPromiseThenable) {
+  let called = false
+
+  const resolve = y => {
+    if (called) return
+
+    procedure(promise, y)
+    called = true
+  }
+  const reject = e => {
+    if (called) return
+
+    promise._reject(e)
+    called = true
+  }
+
+  try {
+    x.then(resolve, reject)
+  } catch (e) {
+    promise._reject(e)
   }
 }
 
@@ -36,9 +99,11 @@ export default class Promise {
    *    > Promise.resolve(<thenable>)
    */
   public static resolve(value): Promise {
-    return new Promise(resolve => {
-      resolve(value)
-    })
+    const promise = new Promise()
+
+    procedure(promise, value)
+
+    return promise
   }
 
   /**
@@ -49,65 +114,59 @@ export default class Promise {
    *    > Promise.resolve(<any>)
    */
   public static reject(value): Promise {
-    return new Promise((resolve, reject) => {
-      reject(value)
-    })
+    const promise = new Promise()
+
+    if (isPromise(value) || isThenable(value)) {
+      procedure(promise, value)
+    } else {
+      promise._reject(value)
+    }
+
+    return promise
   }
 
-  public static all(promises: any[]): Promise {
+  public static all(list: any[]): Promise {
     return new Promise((resolve, reject) => {
-      const states = new Array(promises.length)
-      const values = new Array(promises.length)
+      const states = new Array(list.length)
+      const values = new Array(list.length)
 
-      for (let i = 0, len = promises.length; i < len; i++) {
-        const promise = promises[i]
+      for (let i = 0, len = list.length; i < len; i++) {
+        const item = list[i]
+        const promise = new Promise()
 
-        if (promise instanceof Promise) {
-          promise.then(x => {
-            values[i] = x
-            states[i] = true
+        procedure(promise, item)
 
-            if (states.every(y => !!y)) {
-              resolve(values)
-            }
-          }, reject)
-        } else {
-          values[i] = promise
+        promise.then(x => {
+          values[i] = x
           states[i] = true
-        }
-      }
 
-      if (states.every(y => !!y)) {
-        resolve(values)
+          if (states.every(y => !!y)) {
+            resolve(values)
+          }
+        }, reject)
       }
     })
   }
 
-  public static race(promises: any[]): Promise {
+  public static race(list: any[]): Promise {
     return new Promise((resolve, reject) => {
       let state = false
       let value
 
-      for (let i = 0, len = promises.length; i < len; i++) {
-        const promise = promises[i]
+      for (let i = 0, len = list.length; i < len; i++) {
+        const item = list[i]
+        const promise = new Promise()
 
-        if (promise instanceof Promise) {
-          promise.then(x => {
-            if (state) return
+        procedure(promise, item)
 
-            state = true
-            value = x
-
-            resolve(value)
-          }, reject)
-        } else {
+        promise.then(x => {
           if (state) return
 
           state = true
-          value = promise
+          value = x
 
-          resolve(promise)
-        }
+          resolve(value)
+        }, reject)
       }
     })
   }
@@ -115,7 +174,7 @@ export default class Promise {
   // 存储成功结果值
   public value: any
   // 存储下一个 promise
-  public nextPromise: Promise
+  public nextPromise: Promise = new Promise()
   // 成功回调事件
   public fulfilledCbs: PromiseCallbackListType = []
   // 失败回调事件
@@ -123,6 +182,7 @@ export default class Promise {
   // 当前 Promise 状态
   public state: PromiseStateType = 'pending'
 
+  // FIXME: 有些许不规范，resolver 真实是必需传入项
   public constructor(resolver?) {
     if (resolver && typeof resolver !== 'function')
       throw new Error(`Promise resolver ${resolver} is not a function`)
@@ -137,8 +197,6 @@ export default class Promise {
    * @param reject
    */
   public then(resolve, reject): Promise {
-    this.nextPromise = this.nextPromise || new Promise()
-
     if (typeof resolve === 'function') {
       this.fulfilledCbs.push(resolve)
     }
@@ -178,56 +236,6 @@ export default class Promise {
     this._callbacks()
   }
 
-  private _procedure(promise: Promise, x: any) {
-    if (promise === x) {
-      return promise._reject(new TypeError('promise and value cannot be same'))
-    }
-
-    if (x instanceof Promise) {
-      return this._procedurePromise(promise, x)
-    }
-
-    if (x.then) {
-      return this._procedureThenable(promise, x)
-    }
-
-    return promise._resolve(x)
-  }
-
-  private _procedurePromise(promise: Promise, x: Promise) {
-    if (x.state === 'fulfilled') {
-      promise._resolve(x.value)
-    }
-
-    if (x.state === 'rejected') {
-      promise._reject(x.value)
-    }
-
-    x.then(promise._resolve.bind(promise), promise._reject.bind(promise))
-  }
-
-  private _procedureThenable(promise: Promise, x: IPromiseThenable) {
-    let called = false
-    const resolve = y => {
-      if (called) return
-
-      this._procedure(promise, y)
-      called = true
-    }
-    const reject = e => {
-      if (called) return
-
-      promise._reject(e)
-      called = true
-    }
-
-    try {
-      x.then(resolve, reject)
-    } catch (e) {
-      promise._reject(e)
-    }
-  }
-
   private _callbacks() {
     const queue =
       this.state === 'fulfilled' ? this.fulfilledCbs : this.rejectedCbs
@@ -240,7 +248,7 @@ export default class Promise {
           const x =
             typeof queue === 'function' ? queue[i](this.value) : this.value
 
-          this._procedure(this.nextPromise, x)
+          procedure(this.nextPromise, x)
         } catch (e) {
           this.nextPromise._reject(e)
         }
